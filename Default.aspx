bcf1c996a4843e9a8ef1d539d36048c4b02f83c3
Fatma Koç <fatmakoc94@gmail.com>
2015-06-26 12:05:23 +0300

    okey

---
+++ Default.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+  
+    <form id="form1" runat="server">
+    <div>
+     
+        <asp:Label ID="Label1" runat="server" Text="Araç Sahibi ID"></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Plaka
+        <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Plaka"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+&nbsp;
+        <br />
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="Model"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="Renk"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <br />
+        <br />
+        <asp:CheckBox ID="CheckBox1" runat="server" Text="Aktif" />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Ekle" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Güncelle" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Sil" />
+        <br />
+        <br />
+        <br />
+        <asp:GridView ID="GridView1" runat="server" Width="577px">
+        </asp:GridView>
+        <br />
+        <br />
+        <br />
+        <br />
+     
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+    AracTeknikServisEntities1 arac = new AracTeknikServisEntities1();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        goster();
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        bool aktif = false;
+        int id = Convert.ToInt32(TextBox1.Text);
+        string plk = TextBox2.Text;
+        int mdl = Convert.ToInt32(TextBox3.Text);
+        string rnk = TextBox4.Text;
+        if (CheckBox1.Checked) aktif = true;
+
+        ekle(id,plk,mdl,rnk, aktif); 
+        goster();
+        
+    }
+
+    public void sil(string plaka)
+    {  
+        try
+        {
+           var kytsil=(from k in arac.Aracs where k.Plaka==plaka select k).FirstOrDefault();
+                       
+            arac.Aracs.Remove(kytsil);
+            arac.SaveChanges();
+        }
+        catch (Exception)
+        {
+            Response.Write("HATA");
+        }
+       
+    }
+
+    public void guncelle(int sahipid, string plaka, int modelno, string renk, bool aktif)
+    {
+        try
+        {
+            Arac aracguncelle = (from k in arac.Aracs where k.Plaka == plaka select k).FirstOrDefault();
+            aracguncelle.AracSahibiID = sahipid;
+            aracguncelle.ModelNo = modelno;
+            aracguncelle.Renk = renk;
+            aracguncelle.active = aktif;
+
+            arac.SaveChanges();
+
+        }
+        catch (Exception)
+        {
+            Response.Write("HATA");
+        }
+    }
+
+    public void ekle(int sahipid,string plaka,int modelno,string renk,bool aktif)
+    { 
+        try
+        {
+            Arac araclar = new Arac
+            {
+                AracSahibiID=sahipid,
+                Plaka=plaka,
+                ModelNo=modelno,
+                Renk=renk,
+                active=aktif
+
+            };
+           arac.Aracs.Add(araclar);
+           arac.SaveChanges();
+        }
+        catch (Exception)
+        {
+            Response.Write("HATA");
+        }
+        
+    }
+
+    public void goster()
+    {
+        //Burası istediğimiz alanlar icin
+        //var oku = from c in arac.Aracs
+        //          select new
+        //          {
+        //              c.AracSahibiID,
+        //              c.Plaka,
+        //              c.ModelNo,
+        //              c.Renk,
+        //              c.active
+        //          };
+        //GridView1.DataSource = oku.ToList();
+
+
+       //******* burası tüm alanlar için
+       GridView1.DataSource = arac.Aracs.ToList();
+       GridView1.DataBind();
+
+
+        //**********************Sadece belirli özelliğe göre verileri getirmesi için
+
+        //var rengeGore = from rr in arac.Aracs where rr.Renk=="Lacivert"
+        //                select new
+        //                {
+        //                    rr.AracSahibiID,
+        //                    rr.Plaka,
+        //                    rr.ModelNo,
+        //                    rr.Renk,
+        //                    rr.active
+        //                };
+        //GridView1.DataSource = rengeGore.ToList();
+        //GridView1.DataBind();
+
+
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+
+        bool aktif = false;
+        int id = Convert.ToInt32(TextBox1.Text);
+        string plk = TextBox2.Text;
+        int mdl = Convert.ToInt32(TextBox3.Text);
+        string rnk = TextBox4.Text;
+        if (CheckBox1.Checked) aktif = true;
+
+        guncelle(id, plk, mdl, rnk, aktif);
+        goster();
+        
+    }
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+
+        sil(TextBox5.Text);
+        goster();
+        
+    }
+}
